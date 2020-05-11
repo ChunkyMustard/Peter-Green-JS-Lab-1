22a490d575851ce3a3aa8cff01980f427a2deca7
Peter Green <fishlegs10@gmail.com>
2020-05-11 18:33:08 -0400

    Peter Green JS Lab 1

---
+++ script.js
@@ -0,0 +1,70 @@
+const randomDamage = () =>
+{
+	return Math.floor(Math.random() * 10) + 1;
+}
+
+const chooseOption = (opt1, opt2) =>
+{
+	let randNum = Math.floor(Math.random() * 2);
+	randNum = (randNum < 1) ? opt2:opt1;
+	return randNum;
+}
+
+const attackPlayer = function (health)
+{
+	health = health - randomDamage();
+	return health;
+}
+
+const logHealth = (player, health) =>
+{
+	console.log(player + "'s health: " + health);
+}
+
+const logDeath = (winner, loser) =>
+{
+	console.log(winner + " defeated " + loser);
+}
+
+const isDead = (health) =>
+{
+	if (health <= 0)
+	{
+		return true;
+	} else
+	{
+		return false;
+	}
+}
+
+function fight(player1, player2, player1Health, player2Health)
+{
+	while (isDead(player1Health) == false && isDead(player2Health) == false)
+	{
+		let attacker = chooseOption(player1, player2);
+		
+		if (attacker == player1)
+		{
+			player2Health = attackPlayer(player2Health);
+			logHealth(player2, player2Health);
+			
+			if (isDead(player2Health) == true)
+			{
+				logDeath(player1, player2);
+				break;
+			}
+		} else 
+		{
+			player1Health = attackPlayer(player1Health);
+			logHealth(player1, player1Health);
+			
+			if (isDead(player1Health) == true)
+			{
+				logDeath(player2, player1);
+				break;
+			}
+		}
+	}
+}
+
+fight("Chad", "Stacy", 100, 100);
